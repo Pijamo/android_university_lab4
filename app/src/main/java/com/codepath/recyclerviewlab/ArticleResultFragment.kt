4b430464abcf91a3e83a29a400c005973610a9b7
Pijamo <ngulliejake@gmail.com>
2022-03-13 01:11:55 +0530

    completed

--- app/src/main/java/com/codepath/recyclerviewlab/ArticleResultFragment.kt
+++ app/src/main/java/com/codepath/recyclerviewlab/ArticleResultFragment.kt
@@ -1,5 +1,6 @@
 package com.codepath.recyclerviewlab
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,16 +10,33 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.codepath.recyclerviewlab.R.layout
+import com.codepath.recyclerviewlab.models.Article
+import com.codepath.recyclerviewlab.networking.CallbackResponse
 import com.codepath.recyclerviewlab.networking.NYTimesApiClient
+
+
 
 class ArticleResultFragment: Fragment() {
     private val client = NYTimesApiClient()
+    private val adapter = ArticleResultsRecyclerViewAdapter()
+
+    private lateinit var list: RecyclerView
+    private lateinit var progressSpinner: ContentLoadingProgressBar
+
+
+    private var currentQuery: String? = null
+
+
     override fun onPrepareOptionsMenu(menu: Menu) {
         val item = menu.findItem(R.id.action_search).actionView as SearchView
         item.setOnQueryTextListener(object : OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
+                currentQuery = query
                 loadNewArticlesByQuery(query)
                 return false
             }
@@ -38,16 +56,79 @@ class ArticleResultFragment: Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(layout.fragment_article_result_list, container, false)
+        val view = inflater.inflate(layout.fragment_article_result_list, container, false)
+
+        list = view.findViewById(R.id.list)
+        progressSpinner= view.findViewById(R.id.progress)
+
+        val linearLayoutManager = LinearLayoutManager(view.context)
+        list.layoutManager = linearLayoutManager
+        list.adapter = adapter
+
+        //adding Endless View
+        list.addOnScrollListener(object : EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView?) {
+                loadArticlesByPage(page)
+            }
+        })
+
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
     }
 
     private fun loadNewArticlesByQuery(query: String) {
         Log.d("ArticleResultFragment", "loading articles for query $query")
         Toast.makeText(context, "Loading articles for \'$query\'", Toast.LENGTH_SHORT).show()
-        // TODO(Checkpoint 3): Implement this method to populate articles
+
+        progressSpinner.show()
+
+        client.getArticlesByQuery(
+            articlesListResponse = object : CallbackResponse<List<Article>> {
+                override fun onSuccess(model: List<Article>) {
+                    Log.d(ArticleResultFragment::class.java.simpleName, "Success")
+                    adapter.setNewArticles(newArticles = model)
+                    adapter.notifyDataSetChanged()
+
+                    progressSpinner.hide()
+                }
+
+                override fun onFailure(error: Throwable?) {
+                    Toast.makeText(context, error?.message, Toast.LENGTH_SHORT).show()
+                    Log.d(ArticleResultFragment::class.java.simpleName, "failure")
+                }
+            },
+            query = query
+        )
     }
 
     private fun loadArticlesByPage(page: Int) {
-        // TODO(Checkpoint 4): Implement this method to do infinite scroll
+        client.getArticlesByQuery(
+            articlesListResponse = object : CallbackResponse<List<Article>> {
+                override fun onSuccess(model: List<Article>) {
+                    Log.d(
+                        ArticleResultFragment::class.java.simpleName,
+                        "Success: page - $page for $currentQuery"
+                    )
+                    adapter.addArticles(newArticles = model)
+                    adapter.notifyDataSetChanged()
+                }
+
+                override fun onFailure(error: Throwable?) {
+                    Toast.makeText(context, error?.message, Toast.LENGTH_SHORT).show()
+                    Log.d(
+                        ArticleResultFragment::class.java.simpleName,
+                        "failure to load page: $page"
+                    )
+                }
+            },
+            query = currentQuery,
+            pageNumber = page)
     }
 }
--- app/src/main/java/com/codepath/recyclerviewlab/ArticleResultsRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/recyclerviewlab/ArticleResultsRecyclerViewAdapter.kt
@@ -1,4 +1,61 @@
 package com.codepath.recyclerviewlab
 
-class Adapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.recyclerviewlab.models.Article
+
+// Define a static int for each view type, loading = showing the loading spinner at the end of the list
+const val VIEW_TYPE_LOADING = 0
+const val VIEW_TYPE_ARTICLE = 1
+
+class ArticleResultsRecyclerViewAdapter : RecyclerView.Adapter<ArticleResultsRecyclerViewAdapter.ArticleViewHolder>(){
+
+    private val articles = mutableListOf<Article>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ArticleResultsRecyclerViewAdapter.ArticleViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        val view = inflater.inflate(R.layout.fragment_article_result, parent, false)
+
+        return ArticleViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: ArticleResultsRecyclerViewAdapter.ArticleViewHolder,
+        position: Int
+    ) {
+        val article: Article = articles.get(position)
+
+        holder.article_pub_date.text = article.publishDate
+        holder.article_headline.text = article.headline?.main
+        holder.article_snippet.text = article.snippet
+    }
+
+    override fun getItemCount(): Int {
+        return articles.size
+    }
+
+    fun setNewArticles(newArticles: List<Article>) {
+        articles.clear()
+        articles.addAll(newArticles)
+    }
+
+    fun addArticles(newArticles: List<Article>) {
+        articles.addAll(newArticles)
+    }
+
+    class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val article_pub_date = itemView.findViewById<TextView>(R.id.article_pub_date)
+        val article_headline = itemView.findViewById<TextView>(R.id.article_headline)
+        val article_snippet = itemView.findViewById<TextView>(R.id.article_snippet)
+
+    }
 }
